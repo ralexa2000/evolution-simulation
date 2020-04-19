ba38a1cd908a26dfdb5424b3dd95f2f655d4d413
aryabinina <aryabinina@yandex-team.ru>
2020-04-19 14:29:26 +0300

    initial commit

---
+++ evolve.py
@@ -0,0 +1,124 @@
+import matplotlib.pyplot as plt
+import random
+
+
+SPACE_SIZE = 100
+N_DAYS = 10000
+PAUSE = 0.01
+ALL_DIRECTIONS = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+
+OBJECT_COLORS = {
+    "creature": "#A52A2A",
+    "food": "#ADFF2F"
+}
+
+
+class Object:
+    def __init__(self, object_type):
+        self.type = object_type
+        self.color = OBJECT_COLORS[object_type]
+        self.moved_in_current_round = False
+
+
+class Space:
+    def __init__(self, size):
+        self.size = size
+        self.objects = [[None]*size for _ in range(size)]
+        self.cnt_creatures = 0
+        self.cnt_food = 0
+
+    def add_object(self, x, y, object):
+        self.objects[x][y] = object
+        if object.type == 'creature':
+            self.cnt_creatures += 1
+        if object.type == 'food':
+            self.cnt_food += 1
+
+    def plot_objects(self):
+        x_coordinates = []
+        y_coordinates = []
+        colors = []
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.objects[x][y]:
+                    x_coordinates.append(x)
+                    y_coordinates.append(y)
+                    colors.append(self.objects[x][y].color)
+        plt.scatter(x_coordinates, y_coordinates, c=colors)
+
+    def move_creatures(self):
+        self.clean_moved_in_current_round()
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.objects[x][y] and self.objects[x][y].type == "creature" and \
+                        not self.objects[x][y].moved_in_current_round:
+                    for x_direction, y_direction in ALL_DIRECTIONS:
+                        if self.is_possible_coordinates(x + x_direction, y + y_direction) and \
+                                self.objects[x + x_direction][y + y_direction] and \
+                                self.objects[x + x_direction][y + y_direction].type == "food":
+                            self.objects[x + x_direction][y + y_direction] = self.objects[x][y]
+                            self.objects[x + x_direction][y + y_direction].moved_in_current_round = True
+                            self.objects[x][y] = None
+                            self.cnt_food -= 1
+                            break
+                    else:
+                        x_direction, y_direction = random.choice(self.get_possible_directions(x, y))
+                        if x_direction != 0 or y_direction != 0:
+                            self.objects[x + x_direction][y + y_direction] = self.objects[x][y]
+                            self.objects[x + x_direction][y + y_direction].moved_in_current_round = True
+                            self.objects[x][y] = None
+
+    def clean_moved_in_current_round(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.objects[x][y]:
+                    self.objects[x][y].moved_in_current_round = False
+
+    def get_possible_directions(self, x, y):
+        possible_directions = []
+        for x_direction, y_direction in ALL_DIRECTIONS:
+            if self.is_possible_coordinates(x + x_direction, y + y_direction) and \
+                    (x_direction == 0 and y_direction == 0 or not self.objects[x + x_direction][y + y_direction]):
+                possible_directions.append((x_direction, y_direction))
+        return possible_directions
+
+    def is_possible_coordinates(self, x, y):
+        return 0 <= x < self.size and 0 <= y < self.size
+
+    def get_vacant_position(self):
+        x, y = None, None
+        while x is None or y is None or self.objects[x][y]:
+            x = random.randint(0, self.size - 1)
+            y = random.randint(0, self.size - 1)
+        return x, y
+
+
+plt.ion()
+plt.figure(figsize=(10, 10))
+
+space = Space(SPACE_SIZE)
+
+# fill food
+for i in range(SPACE_SIZE * 3):
+    x, y = space.get_vacant_position()
+    space.add_object(x, y, Object("food"))
+
+# fill creatures
+for i in range(SPACE_SIZE // 10):
+    x, y = space.get_vacant_position()
+    space.add_object(x, y, Object("creature"))
+
+for day in range(N_DAYS):
+    if day // 5 == 0:
+        x, y = space.get_vacant_position()
+        space.add_object(x, y, Object("food"))
+
+    space.move_creatures()
+    space.plot_objects()
+
+    print(space.cnt_creatures, space.cnt_food)
+
+    plt.xlim(0, SPACE_SIZE)
+    plt.ylim(0, SPACE_SIZE)
+    plt.pause(PAUSE)
+    plt.clf()
