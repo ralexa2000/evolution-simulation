e48e15307f3e3fbba9e409c32125f07b18cf1741
aryabinina <aryabinina@yandex-team.ru>
2020-04-19 18:08:02 +0300

    fix counters

--- evolve.py
+++ evolve.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import matplotlib.pyplot as plt
 import random
 
@@ -7,16 +8,23 @@ N_DAYS = 10000
 PAUSE = 0.01
 ALL_DIRECTIONS = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
-OBJECT_COLORS = {
-    "creature": "#A52A2A",
-    "food": "#ADFF2F"
+POSSIBLE_OBJECTS = {
+    'creature': {
+        'color': '#A52A2A'
+    },
+    'food': {
+        'color': '#ADFF2F'
+    }
 }
 
 
 class Object:
     def __init__(self, object_type):
+        if object_type not in POSSIBLE_OBJECTS:
+            raise TypeError('Object type {} is not implemented'.format(object_type))
+        
         self.type = object_type
-        self.color = OBJECT_COLORS[object_type]
+        self.color = POSSIBLE_OBJECTS[object_type]['color']
         self.moved_in_current_round = False
 
 
@@ -24,15 +32,11 @@ class Space:
     def __init__(self, size):
         self.size = size
         self.objects = [[None]*size for _ in range(size)]
-        self.cnt_creatures = 0
-        self.cnt_food = 0
+        self.cnt_objects = defaultdict(int)
 
-    def add_object(self, x, y, object):
-        self.objects[x][y] = object
-        if object.type == 'creature':
-            self.cnt_creatures += 1
-        if object.type == 'food':
-            self.cnt_food += 1
+    def add_object(self, x, y, object_type):
+        self.objects[x][y] = Object(object_type)
+        self.cnt_objects[object_type] += 1
 
     def plot_objects(self):
         x_coordinates = []
@@ -50,16 +54,16 @@ class Space:
         self.clean_moved_in_current_round()
         for x in range(self.size):
             for y in range(self.size):
-                if self.objects[x][y] and self.objects[x][y].type == "creature" and \
+                if self.objects[x][y] and self.objects[x][y].type == 'creature' and \
                         not self.objects[x][y].moved_in_current_round:
                     for x_direction, y_direction in ALL_DIRECTIONS:
                         if self.is_possible_coordinates(x + x_direction, y + y_direction) and \
                                 self.objects[x + x_direction][y + y_direction] and \
-                                self.objects[x + x_direction][y + y_direction].type == "food":
+                                self.objects[x + x_direction][y + y_direction].type == 'food':
                             self.objects[x + x_direction][y + y_direction] = self.objects[x][y]
                             self.objects[x + x_direction][y + y_direction].moved_in_current_round = True
                             self.objects[x][y] = None
-                            self.cnt_food -= 1
+                            self.cnt_objects['food'] -= 1
                             break
                     else:
                         x_direction, y_direction = random.choice(self.get_possible_directions(x, y))
@@ -101,22 +105,22 @@ space = Space(SPACE_SIZE)
 # fill food
 for i in range(SPACE_SIZE * 3):
     x, y = space.get_vacant_position()
-    space.add_object(x, y, Object("food"))
+    space.add_object(x, y, 'food')
 
 # fill creatures
 for i in range(SPACE_SIZE // 10):
     x, y = space.get_vacant_position()
-    space.add_object(x, y, Object("creature"))
+    space.add_object(x, y, 'creature')
 
 for day in range(N_DAYS):
     if day // 5 == 0:
         x, y = space.get_vacant_position()
-        space.add_object(x, y, Object("food"))
+        space.add_object(x, y, 'food')
 
     space.move_creatures()
     space.plot_objects()
 
-    print(space.cnt_creatures, space.cnt_food)
+    print(space.cnt_objects)
 
     plt.xlim(0, SPACE_SIZE)
     plt.ylim(0, SPACE_SIZE)
